a5afa5cb3e5b515cbcc841ba4f81b3f22e0f365a
Ariful Islam <arifulgsc01@gmail.com>
2024-10-20 02:42:03 +0600

    Add project files.

---
+++ WebApiMasterDetailWithAuthentication/Migrations/202405301113084_next.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApiMasterDetailWithAuthentication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class next : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Email = c.String(),
+                        Roles = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
